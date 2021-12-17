3db5cfd8e8018047d2cf11cc6bb8356cf71e3363
Евгений <@DESKTOP-11IJB3S>
2021-12-17 09:41:43 +0300

    Добавьте файлы проекта.

---
+++ SLW16/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace SLW16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(12, 95);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(1237, 720);
+            this.zedGraph.TabIndex = 0;
+            this.zedGraph.UseExtendedPrintDialog = true;
+            this.zedGraph.Load += new System.EventHandler(this.zedGraph_Load);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BUILD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 827);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ SLW16/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+using System.Runtime;
+
+namespace SLW16
+{
+    public partial class Form1 : Form
+    {
+        private double _a;
+        private double _b;
+        private double _dx;
+        private bool isBuilt = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private double Func(double x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            return Math.Pow(x - 2, 2.5) + _a * _b;
+        }
+
+        private void DrawGraph()
+        {
+            GraphPane pane = zedGraph.GraphPane;
+            Pen pen = new Pen(Color.Blue, 1);
+            pane.CurveList.Clear();
+            pane.GraphObjList.Clear();
+            PointPairList list = new PointPairList();
+
+            double xmin = 1;
+            double xmax = 4;
+
+            for (double x = xmin; x <= xmax; x += _dx)
+            {
+                list.Add(x, Func(x));
+            }
+
+            LineItem myCurve = pane.AddCurve("Sinc", list, Color.Blue, SymbolType.None);          
+            TextObj text1X = new TextObj("1", 2, 0);
+            text1X.FontSpec.Size = 6f;
+            text1X.FontSpec.Border.IsVisible = false;
+            pane.GraphObjList.Add(text1X);
+            TextObj text2X = new TextObj("4", 4, 0);
+            text2X.FontSpec.Size = 6f;
+            text2X.FontSpec.Border.IsVisible = false;
+            pane.GraphObjList.Add(text2X);
+
+            TextObj text1Y = new TextObj(Convert.ToString(Func(2)), 0, Func(2));
+            text1Y.FontSpec.Size = 6f;
+            text1Y.FontSpec.Border.IsVisible = false;
+            pane.GraphObjList.Add(text1Y);
+            TextObj text2Y = new TextObj(Convert.ToString(Func(4)), 0, Func(4));
+            text2Y.FontSpec.Size = 6f;
+            text2Y.FontSpec.Border.IsVisible = false;
+            pane.GraphObjList.Add(text2Y);
+
+            LineObj lineY = new LineObj(Color.Black, 0, -1000, 0, 1000);
+            pane.GraphObjList.Add(lineY);
+
+            LineObj line1X = new LineObj(Color.Red, 2, 0, 2, Func(2));
+            pane.GraphObjList.Add(line1X);
+            LineObj line2X = new LineObj(Color.Red, 4, 0, 4, Func(4));
+            pane.GraphObjList.Add(line2X);
+
+            LineObj line1Y = new LineObj(Color.Green, 0, Func(2), 2, Func(2));
+            pane.GraphObjList.Add(line1Y);
+            LineObj line2Y = new LineObj(Color.Green, 0, Func(4), 4, Func(4));
+            pane.GraphObjList.Add(line2Y);
+
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _a = Convert.ToDouble(textBox1.Text);
+                _b = Convert.ToDouble(textBox2.Text);
+                _dx = 0.001;
+                DrawGraph();
+                isBuilt = true;
+            }
+            catch
+            {
+                MessageBox.Show("Set the boxes with values: a | b");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (isBuilt)
+            {
+                try
+                {
+                    _a = Convert.ToDouble(textBox1.Text);
+                    _b = Convert.ToDouble(textBox2.Text);
+                    DrawGraph();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (isBuilt)
+            {
+                try
+                {
+                    _a = Convert.ToDouble(textBox1.Text);
+                    _b = Convert.ToDouble(textBox2.Text);
+                    DrawGraph();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void zedGraph_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
